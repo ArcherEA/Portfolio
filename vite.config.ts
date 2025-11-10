d234bcb9862b62ea3d55622c520409ed581770ed
ArcherEA <haogenmao@gmail.com>
2025-11-10 14:29:18 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   base: '/Portfolio/',
+  server: {
+    allowedHosts: [
+      'yukuan.dpdns.org'
+    ],
 });
